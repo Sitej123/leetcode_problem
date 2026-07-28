f41894a7067a8a3f72b252edf675723a1ef63dfc
Sitej Meher <168086510+Sitej123@users.noreply.github.com>
2026-07-28 23:06:04 +0530

    Time: 1 ms (99.97%) | Memory: 42.8 MB (26.77%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,7 +1,3 @@
-// Source - https://stackoverflow.com/a/59029832
-// Posted by Eran, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-07-28, License - CC BY-SA 4.0
-
 class Solution {
     public int reverse(int x) {
         long ans = 0;
@@ -12,7 +8,7 @@ class Solution {
         if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE) {
             return 0;
         } else {
-            return (int) ans;
+            return (int)ans;
         }
     }
 }

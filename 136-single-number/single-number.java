eb0fc264c1377ce37a4daa3bfd03e16c1f018b4f
Sitej Meher <168086510+Sitej123@users.noreply.github.com>
2026-08-19 23:26:38 +0530

    Time: 1 ms (99.94%) | Memory: 46.7 MB (93.01%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,19 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int i=0;
-        int count;
-        while(i<nums.length){
-            count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count==1){
-                return nums[i];
-            }
-            i++;
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
         }
-        return 0;
+
+        return ans;
     }
 }
